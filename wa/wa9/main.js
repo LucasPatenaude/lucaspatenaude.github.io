d39aec60452281db74d25af3858ae1939ee9d7d3
Lucas Patenaude <lucaspatenaude@gmail.com>
2023-11-02 05:04:41 -0600

    JavaScript Finished and Working

---
+++ wa/wa9/main.js
@@ -0,0 +1,56 @@
+// Get references to HTML elements
+const customName = document.getElementById('customname'); // Get element with ID 'customname'
+const randomize = document.querySelector('.randomize'); // Get first element with class 'randomize'
+const story = document.querySelector('.story'); // Get first element with class 'story'
+
+// Generate a random value from an array
+function randomValueFromArray(array) 
+{
+    const random = Math.floor(Math.random() * array.length); // Generate a random index within the array's length
+    return array[random]; // Return the element at the randomly generated index
+}
+
+// Define the story template and arrays for placeholders
+const storyText = 'Once upon a time :insertw: was really bored. Suddenly :insertw: had an idea "I should go to :insertx:, that would be great!" :insertw: called up all its friend, Nosferatu and told them, ":inserty:." Nosferatu replied, ":insertz:."' ;
+const insertW = ['Smaug', 'John Denver', 'Chris Cringle', 'Bono', 'Ozzy Osbourne'];
+const insertX = ['the bathroom', 'church', 'Denver', 'Death Row' ]
+const insertY = ['I\'m going to the bathroom', 'I\'m going to church', 'I\'m going to Denver', "I\'m visiting our local Death Row", "I\'m going to Imaginationland", "Is this the Krusty Krab?", "I weigh 450 pounds"];
+const insertZ = ['Wow! What a great idea', 'That\'s the worst thing I\'ve ever heard', '[SCREAMING NOISES]', "No, This is Patrick"];
+
+// Add an event listener to the 'randomize' button
+randomize.addEventListener('click', result);
+
+// Function to generate and display the story
+function result() 
+{
+    let newStory = storyText; // Initialize newStory with the original story template
+
+    // Get random items from arrays for placeholders
+    const wItem = randomValueFromArray(insertW);
+    const xItem = randomValueFromArray(insertX);
+    const yItem = randomValueFromArray(insertY);
+    const zItem = randomValueFromArray(insertZ);
+
+    // Replace placeholders in the story template with randomly selected items
+    newStory = newStory.replaceAll(':insertw:', wItem);
+    newStory = newStory.replaceAll(':insertx:', xItem);
+    newStory = newStory.replaceAll(':inserty:', yItem);
+    newStory = newStory.replaceAll(':insertz:', zItem);
+
+    // If a custom name is provided, replace 'Bob' in the story with the custom name
+    if (customName.value !== '') // If query is not empty
+    {
+        const name = customName.value;
+        newStory = newStory.replaceAll('Nosferatu', name); // Replace Nosferatu text with custom input
+    }
+
+    // If 'UK' checkbox is checked, convert temperature and weight units
+    if (document.getElementById("uk").checked) 
+    {
+        const weight = `${Math.round(450 * 0.0714286)} stone`; // 1 Stone = 0.0714286
+        newStory = newStory.replaceAll('450 pounds', weight); // Replace pounds text with value of weight
+    }
+
+    story.textContent = newStory; // Update the content of the 'story' element with the generated story
+    story.style.visibility = 'visible'; // Make the 'story' element visible
+}
