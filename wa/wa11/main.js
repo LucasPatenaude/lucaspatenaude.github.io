747f43e26e838fabcfe2e31e9127cf36be2bebf2
Lucas Patenaude <lucaspatenaude@gmail.com>
2023-11-12 17:44:37 -0700

    wa11 Mostly Working

--- wa/wa11/main.js
+++ wa/wa11/main.js
@@ -1,15 +1,13 @@
+// Assuming pictures is an array containing image filenames
+const pictures = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
-
-/* Declaring the array of image filenames */
-pictures[5] = "pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg";
+const darkenButton = document.querySelector('.dark');
+const overlay = document.querySelector('.overlay'); // Define overlay
 
 /* Declaring the alternative text for each image file */
-const altText = 
-{
+const altText = {
     pic1: "Beautiful landscape with mountains and a lake",
     pic2: "Close-up of a cute kitten",
     pic3: "Abstract artwork with vibrant colors",
@@ -20,27 +18,33 @@ const altText =
 
 /* Looping through images */
 
-// Loop through the filenames and create thumbnail images
-filenames.forEach((filename, index) => 
-{
+pictures.forEach((filename, index) => {
     const imgElement = document.createElement('img');
-    imgElement.src = filename;
-    imgElement.alt = altText[`image${index + 1}`]; // Assuming filenames correspond to altText keys
-    imgElement.classList.add('thumbnail');
+    imgElement.src = `images/${filename}`; // Assuming images are in the "images" directory
+    imgElement.alt = altText[`pic${index + 1}`]; // Fixed altText key
+    imgElement.classList.add('thumbnail'); // Add a class for styling
 
     // Add click event listener to switch the displayed image
-    imgElement.addEventListener('click', () => 
-    {
-        displayedImg.src = filename;
-        displayedImg.alt = altText[`image${index + 1}`];
+    imgElement.addEventListener('click', () => {
+        displayedImage.src = imgElement.src;
+        displayedImage.alt = imgElement.alt;
     });
 
     thumbBar.appendChild(imgElement);
 });
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+/* Adding a click event listener to the button */
 
-/* Wiring up the Darken/Lighten button */
+darkenButton.addEventListener('click', () => {
+    const currentClass = darkenButton.getAttribute('class');
+    
+    if (currentClass === 'dark') {
+        darkenButton.setAttribute('class', 'light');
+        darkenButton.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+    } else {
+        darkenButton.setAttribute('class', 'dark');
+        darkenButton.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+    }
+});
