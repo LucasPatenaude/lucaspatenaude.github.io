0a038c2d60cac4d4c51f16f54ac2bb123c9fb77a
Lucas Patenaude <lucaspatenaude@gmail.com>
2023-12-03 00:08:01 -0700

    Timer Added + Code/File Reorganization

--- javascript-final/js/numbers.js
+++ javascript-final/js/numbers.js
@@ -8,7 +8,21 @@ function handleNumberClick(number)
   inputElement.value += number;
 }
 
-// Function to display random numbers on the page
+// Function to generate a random interval value between 1 and 10 and call displayNumbers
+function generateRandomInterval() 
+{
+  // Generate a random value between 1 and 10, then append "000" to it
+  var intervalValue = Math.floor(Math.random() * 7) + 1;
+  intervalValue = intervalValue * 1000; // Append "000"
+
+  // Call displayNumbers with the new interval value
+  displayNumbers();
+
+  // Set an interval to run the function with the new interval value
+  setInterval(displayNumbers, intervalValue);
+}
+
+// Function to display random numbers on the page with random positions
 function displayNumbers() 
 {
   // Your array of numbers
@@ -23,7 +37,7 @@ function displayNumbers()
   // Generate unique random numbers
   for (var i = 0; i < 10; i++) 
   {
-    var randomNumber;
+    let randomNumber;
 
     // Generate a random number between 0 and 9
     do 
@@ -33,34 +47,35 @@ function displayNumbers()
 
     // Add the unique random number to the array
     numbers.push(randomNumber);
-  }
 
-  // Iterate through the numbers array and append each number to the output element
-  numbers.forEach(function (number) 
-  {
     // Create a new <span> element for each number
     var span = document.createElement('span');
 
     // Set the text content of the <span> to the current number
-    span.textContent = number;
+    span.textContent = randomNumber;
 
     // Add the "numbers" class to the <span> element
     span.classList.add('numbers');
+
+    // Set random position for the <span> element within the visible area of the content container
+    var contentContainer = document.getElementById('content');
+    var randomX = Math.random() * (contentContainer.clientWidth); // Adjust as per your span element width
+    var randomY = Math.random() * (contentContainer.clientHeight); // Adjust as per your span element height
+    span.style.position = 'absolute';
+    span.style.left = randomX + 'px';
+    span.style.top = randomY + 'px';
 
     // Add a click event listener to each number
     span.addEventListener('click', function () 
     {
       // Call the function to handle the click event
-      handleNumberClick(number);
+      handleNumberClick(randomNumber);
     });
 
     // Append the <span> to the output element
     outputElement.appendChild(span);
-  });
+  }
 }
 
 // Call the function to display numbers when the page loads
-displayNumbers();
-
-// Set an interval to run the function every 3 seconds
-setInterval(displayNumbers, 3000);
+generateRandomInterval();
